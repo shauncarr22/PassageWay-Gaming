2bbd32ca94d74c999738f415f6c6d6df24c26ffa
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-06 12:47:08 -0700

    css signup added

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -14,19 +14,31 @@ const Signup = () => {
     // if(isAuth) reRoute.push('/profile');
 
     return(
-        <div>
-            <p className="UserName">Username</p>
-            <input className="Username_input" type="text" name="username" placeholder="Lask1ey" value="username"></input>
-            <p className="Email">Email</p>
-            <input className="Email_input" type="text" name="email" placeholder="usersemail@gmail.com" value="email"></input>
-            <p className="Password">Password</p>
-            <input className="Password_input" type='text' name="password" placeholder="ExaMplePass222!!" value='password'></input>
-            <p className="Twitch">Twitch</p>
-            <input className="Twitch_input" type='text' name='twitch' placeholder='twitch' value='twitch'></input>
-            <p className="Youtube">Youtube</p>
-            <input className="Youtube_input" type="text" name="youtube" placeholder='youtube' value='yotube'></input>
-            <p className="Game">Game currently playing</p>
-            <input className='Game_input' type='text' name='game' placeholder='name a game uwu' value='game'></input>
+        <div className='Container_SignUp'>
+            <div>
+                <p className="UserName">Username</p>
+                <input className="Username_input" type="text" name="username" placeholder="Lask1ey" value="username"></input>
+            </div>
+            <div>
+                <p className="Email">Email</p>
+                <input className="Email_input" type="text" name="email" placeholder="usersemail@gmail.com" value="email"></input>
+            </div>
+            <div>
+                <p className="Password">Password</p>
+                <input className="Password_input" type='text' name="password" placeholder="ExaMplePass222!!" value='password'></input>
+            </div>
+            <div>
+                <p className="Twitch">Twitch</p>
+                <input className="Twitch_input" type='text' name='twitch' placeholder='twitch' value='twitch'></input>
+            </div>
+            <div>
+                <p className="Youtube">Youtube</p>
+                <input className="Youtube_input" type="text" name="youtube" placeholder='youtube' value='yotube'></input>
+            </div>
+            <div>
+                <p className="Game">Game currently playing</p>
+                <input className='Game_input' type='text' name='game' placeholder='name a game uwu' value='game'></input>
+            </div>
         </div>
     );
 
