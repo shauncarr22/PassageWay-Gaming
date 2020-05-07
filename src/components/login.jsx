f9cbdc4112d9df1fb42d9e10a2f671aeed889f5a
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-07 13:05:26 -0700

    login firebase progress

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,24 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component, useContext, useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+import firebase from '../firebase';
+import { AuthContext } from '../AuthContext.jsx'
 
-class Login extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
+const Login = () => {
+    let reRoute = useHistory();
+    const context = useContext(AuthContext);
+    const { isAuth, loggedIn } = context;
 
+    if(isAuth) reRoute.push('/profile');
+
+    const [email,setEmail] = useState("");
+    const [emailClass, setEmailClass] = useState('');
+    const [password, setPassword] = useState('');
+    const [pwClass, setPwClass] = useState('');
+    
+    const [newUser,setNewUserStatus] = useState(false);
+
+    const authenticater = (email, password) => {
+        if(email.length > 0 && password.length > 0){
+            let prom = new Promise ((resolve, reject) => {
+                resolve(firebase.login(email,password))
+            })
+            prom.then(() => {
+                let info = firebase.auth.currentUser.uid
+                loggedIn(info)
+            }).catch(error => {
+                console.error(error);
+                error.code === "auth/user-not-found" ? setEmailClass("email-error") : setEmailClass("email");
+                error.code === "auth/wrong-password" ? setPwClass('password-error') : setPwClass('password');
+            });
         };
     };
+    if(newUser === true){
+        return <Redirect push to="/signup"></Redirect>
+    } else {
+        return (
+                <div>
+                    <img className="Logo_login" src='LogoNew.png' alt=""></img>
 
-    render(){
-        return(
-            <div className="Container_login">
-                <p className="Email_login">Email</p>
-                <input className="Email_login_input"></input>
-                <p className="Pass_login">Password</p>
-                <input className="Pass_login_input"></input>
-                <button className="button_login">Login uwu</button>
-            </div>
-        );
-    };
+                <div className="Container_login">
+                    <p className="Email_login">Email</p>
+                    <input className="Email_login_input"></input>
+                    <p className="Pass_login">Password</p>
+                    <input className="Pass_login_input"></input>
+                    <button className="button_login">Login uwu</button>
+                </div>
+            </div> 
+        )
+    }
 };
 
 export default Login
+
+
+
