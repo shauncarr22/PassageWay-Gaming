91118891ed923dc4dbf20c587ddcdc8ca4145046
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-11 11:18:44 -0700

    route fix

--- src/components/loggedinhome.jsx
+++ src/components/loggedinhome.jsx
@@ -29,7 +29,7 @@ const LoggedHomePage = () => {
     const getUser = (userEmail) => {
         console.log(userEmail);
         setEmail('laskey@gmail.com')
-        Axios.post(`https://passageway-gaming.herokuapp.com/getPost/`, {
+        Axios.post(`https://passageway-gaming.herokuapp.com/getUer/`, {
             email: userEmail
         })
         .then((data) => {
