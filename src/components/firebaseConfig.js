b3bc9917d4c75e454d41ffe26d868fcadcd3de6a
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-11 09:33:28 -0700

    loggedin fix

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD068Zne-cD8XueFluMv6a-2ODIBer3C8w",
+    authDomain: "passgeway-gaming.firebaseapp.com",
+    databaseURL: "https://passgeway-gaming.firebaseio.com",
+    projectId: "passgeway-gaming",
+    storageBucket: "passgeway-gaming.appspot.com",
+    messagingSenderId: "415313406812",
+    appId: "1:415313406812:web:87387776ffb09370676f99",
+    measurementId: "G-HDMJ7ZV7T1"
+};
+
+export default firebaseConfig
