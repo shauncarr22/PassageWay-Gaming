0327a4689ca4c26c6f5fab84e1248bf4f52a584c
shauncarr22 <shauncarr2010@hotmail.com>
2020-05-13 14:55:06 -0700

    server work

--- server/routes/createPos.js
+++ server/routes/createPos.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const postNew = require('../../db/seeds/post');
 
-router.post('/', cors(), (req,res) => {
+router.post('/', (req,res) => {
     MongoClient.connect(`mongodb+srv://ElinkTeam:${password}@home-post-fub39.mongodb.net/test?retryWrites=true&w=majority`, (err,client) => {
         if(err) console.error(err);
         const {postAuthor, post} = req.body;
